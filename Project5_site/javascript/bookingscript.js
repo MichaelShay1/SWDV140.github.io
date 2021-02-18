ec825cee815c99158a1dfa1880608bd67b40272f
MichaelShay1 <73658651+MichaelShay1@users.noreply.github.com>
2021-02-18 08:34:16 -0700

    Add files via upload

---
+++ Project5_site/javascript/bookingscript.js
@@ -0,0 +1,74 @@
+//Booking form javascript
+"use strict";
+
+const $ = selector => document.querySelector(selector);
+
+const displayErrorMsgs = msgs => {
+    // create a new ul element
+    const ul = document.createElement("ul");
+    ul.classList.add("messages");
+
+    // create a new li element for each error message, add to ul
+    for (let msg of msgs) {
+        const li = document.createElement("li");
+        const text = document.createTextNode(msg);
+        li.appendChild(text);
+        ul.appendChild(li);
+    }
+
+    // if ul node isn't in document yet, add it
+    const node = $("ul");
+    if (node == null) {
+        // get the form element 
+        const form = $("form");
+
+        // add ul to parent of form, before the form
+        form.parentNode.insertBefore(ul, form);
+    } else {
+        // replace existing ul node
+        node.parentNode.replaceChild(ul, node);
+    }  
+}
+
+const processEntries = () => {
+    // get form controls to check for validity
+    const email = $("#email_address");
+    const terms = $("#terms");
+    const fname = $("#first_name")
+    const lname = $("#last_name")
+
+    // create array for error messages
+    const msgs = [];
+
+    // check user entries for validity
+    if(fname.value =="" || lname.value =="") {
+        msgs[msgs.length] = "Please enter your first and last name.";
+    }
+    if (email.value == "") {
+        msgs[msgs.length] = "Please enter an email address.";
+    } 
+    if (terms.checked == false) {
+        msgs[msgs.length] = "You must agree to the terms of service."; 
+    }
+
+    // submit the form or notify user of errors
+    if (msgs.length == 0) {  // no error messages
+        $("form").submit(); 
+    } else {
+        displayErrorMsgs(msgs);
+    }
+};
+const resetForm = () => {
+    $("form").reset();
+    
+    // remove error messages
+    $("ul").remove();
+    
+    $("#email_address").focus();
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    $("#contact").addEventListener("click", processEntries);
+    $("#reset_form").addEventListener("click", resetForm);  
+    $("#email_address").focus();   
+});
